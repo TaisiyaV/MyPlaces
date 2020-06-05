99b7c272fccdd3f382ea28cd4c7f667c5c0e4d6b
TasyaV <tasya-vas@mail.ru>
2020-06-05 11:22:52 +0300

    Map view controller

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by tasya on 05.06.2020.
+//  Copyright © 2020 Taisiya V. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
